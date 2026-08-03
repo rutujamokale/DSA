e9f630c7cee43bff567261dc03f6e3c08eafa0d3
rutujamokale <rutujamokale2003@gmail.com>
2026-08-03 13:46:34 +0530

    array

---
+++ ArrayApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+public class Program{
+    public static void Main(string [] args){
+
+        string [] names={"Rutaa","Nikita","Pranta","Sanika"};
+
+        for(int i=0;i<names.Length;i++){
+            Console.WriteLine(names[i]);
+        }
+    }
+}
