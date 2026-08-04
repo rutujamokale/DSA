1ca776af0ebc6484ff71c57768616e19bebb5716
rutujamokale <rutujamokale2003@gmail.com>
2026-08-04 11:41:45 +0530

    sum of two array

--- ArrayApp/Program.cs
+++ ArrayApp/Program.cs
@@ -2,10 +2,36 @@
 public class Program{
     public static void Main(string [] args){
 
-        string [] names={"Rutaa","Nikita","Pranta","Sanika"};
+        // string [] names={"Rutaa","Nikita","Pranta","Sanika"};
 
-        for(int i=0;i<names.Length;i++){
-            Console.WriteLine(names[i]);
-        }
+        // for(int i=0;i<names.Length;i++){
+        //     Console.WriteLine(names[i]);
+        // }
+
+
+         int [,] arr={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+         };
+
+         for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                Console.Write(arr[i,j] + "");
+            }
+            Console.WriteLine("\n");
+         }
+
+         //addition of two array
+
+         int [] array1={2,3,4,5};
+         int [] array2={4,5,6,7};
+         int [] sum= new int[4];
+
+         Console.WriteLine("1D addition");
+         for(int i=0;i<array.length;i++){
+             sum[i]=array1[i] + array[i];
+             Console.Write(sum[i] + " ");
+         }
     }
 }
--- ArrayApp/obj/Debug/net9.0/ArrayApp.AssemblyInfo.cs
+++ ArrayApp/obj/Debug/net9.0/ArrayApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9851d18ac322e401cb7771eac041c77aeea01a63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f630c7cee43bff567261dc03f6e3c08eafa0d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
