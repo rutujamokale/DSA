12732e6fdb5b7d3a0fdf94a042ee9a28eb86d031
rutujamokale <rutujamokale2003@gmail.com>
2026-08-05 11:35:06 +0530

    transpose of array

--- ArrayApp/Program.cs
+++ ArrayApp/Program.cs
@@ -8,72 +8,95 @@ public class Program{
         //     Console.WriteLine(names[i]);
         // }
 
-
+        //Transpose of array
          int [,] arr={
             {1,2,3},
             {4,5,6},
             {7,8,9}
          };
 
-         for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                Console.Write(arr[i,j] + "");
+
+         int[,] transpose=new int[3,3];
+
+         for(int i=0;i<GetLength(0);i++){
+            for(int j=0;i<GetLength(1);j++){
+            transpose[j,i]=arr[i,j];
+
+         } 
+         }
+
+         Console.Write("transpose matrix");
+
+
+         for(int i=0;i<transpose.GetLength(0);i++){
+            for(int j=0;j<transpose.GetLength(1);j++){
+                Console.Write(transpose[i,j] + " ");
             }
-            Console.WriteLine("\n");
+
+            Console.Writeline();
+
          }
 
-         //addition of two array
+        //  for(int i=0;i<3;i++){
+        //     for(int j=0;j<3;j++){
+        //         Console.Write(arr[i,j] + "");
+        //     }
+        //     Console.WriteLine("\n");
+        //  }
 
-         int [] array1={2,3,4,5};
-         int [] array2={4,5,6,7};
-         int [] sum= new int[4];
+        //  //addition of two array
 
-         Console.WriteLine("1D addition");
-         for(int i=0;i<array1.Length;i++){
-             sum[i]=array1[i] + array2[i];
-             Console.Write(sum[i] + " ");
-         }
+        //  int [] array1={2,3,4,5};
+        //  int [] array2={4,5,6,7};
+        //  int [] sum= new int[4];
 
-         int[,] arr1={
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
+        //  Console.WriteLine("1D addition");
+        //  for(int i=0;i<array1.Length;i++){
+        //      sum[i]=array1[i] + array2[i];
+        //      Console.Write(sum[i] + " ");
+        //  }
+
+        //  int[,] arr1={
+        //     {1,2,3},
+        //     {4,5,6},
+        //     {7,8,9}
             
-         };
+        //  };
 
-         int[,] arr2={
-            {2,4,5},
-            {1,4,2},
-            {2,6,5}
-         };
+        //  int[,] arr2={
+        //     {2,4,5},
+        //     {1,4,2},
+        //     {2,6,5}
+        //  };
 
-         int [,] resultArr={
-            {3,6,8},
-            {5,9,8},
-            {9,14,14}
-         };
-            int[,] result= new int[3,3];
-            Console.WriteLine("\n2D array addition");
-            Console.WriteLine(arr1.Length +"=array1 length");
+        //  int [,] resultArr={
+        //     {3,6,8},
+        //     {5,9,8},
+        //     {9,14,14}
+        //  };
+        //     int[,] result= new int[3,3];
+        //     Console.WriteLine("\n2D array addition");
+        //     Console.WriteLine(arr1.Length +"=array1 length");
 
 
-            for(int i=0;i<arr1.GetLength(0);i++){
-                for(int j=0;j<arr1.GetLength(1);j++){
-                result[i,j]=arr1[i,j]+arr2[i,j];
-                Console.Write(result[i,j] + "");
-            }
-            Console.Write("\n");
+        //     for(int i=0;i<arr1.GetLength(0);i++){
+        //         for(int j=0;j<arr1.GetLength(1);j++){
+        //         result[i,j]=arr1[i,j]+arr2[i,j];
+        //         Console.Write(result[i,j] + "");
+        //     }
+        //     Console.Write("\n");
 
-            }
+        //     }
 
-            Console.WriteLine("given array is:");
+        //     Console.WriteLine("given array is:");
 
-            for(int i=0;i<3;i++){
-                for(int j=0;j<3;j++){
-                    Console.Write(arr2[i,j]);
-                }
-                    Console.Write("\n");
-            }
+        //     for(int i=0;i<3;i++){
+                
+        //         for(int j=0;j<3;j++){
+        //             Console.Write(arr2[i,j]);
+        //         }
+        //             Console.Write("\n");
+        //     }
 
 }
 }
